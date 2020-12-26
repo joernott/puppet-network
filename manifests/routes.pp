b22d30c4a8d307883ecf1de1b0967fb1f7969e32
Joern Ott <joern.ott@ott-consult.de>
2020-12-26 17:55:07 +0000

    fix routes

--- manifests/routes.pp
+++ manifests/routes.pp
@@ -1,25 +1,28 @@
 class network::routes(
-  String  $interface,
-  Boolean $restart   = true,
+  Array[String]  $interfaces,
+  Boolean        $restart   = true,
 ) {
 
-  $filename = "/etc/sysconfig/network-scripts/route-${interface}"
   if $restart {
-      $notify = [Service['network']]
+    $notify = [Service['network']]
   } else {
-      $notify = []
-  }
-  
-  concat{"/etc/sysconfig/network-scripts/route-${interface}":
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    notify => $notify,
+    $notify = []
   }
 
-  concat::fragment{ "header_/etc/sysconfig/network-scripts/route-${interface}":
-    target  => $filename,
-    content => "# This file is managed by puppet",
-    order   => '01',
+  $interfaces.each|$interface| {
+    $filename = "/etc/sysconfig/network-scripts/route-${interface}"
+  
+    concat{$filename:
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      notify => $notify,
+    }
+
+    concat::fragment{ "header_${filename}":
+      target  => $filename,
+      content => "# This file is managed by puppet",
+      order   => '01',
+    }
   }
 }
